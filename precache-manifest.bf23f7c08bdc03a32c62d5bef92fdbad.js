c48a14edeba57783c070c1a3c3eab63eab5214f8
hosam faour <hosamf1992@gmail.com>
2019-12-31 22:55:41 +0200

    Updates

---
+++ precache-manifest.bf23f7c08bdc03a32c62d5bef92fdbad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3bd7cf314e32d2a3b2a63a0afab8d5b",
+    "url": "​https://hosamf1992.github.io/Weather-App​/index.html"
+  },
+  {
+    "revision": "f51ba60604725e16efa2",
+    "url": "​https://hosamf1992.github.io/Weather-App​/static/css/main.a09bae5c.chunk.css"
+  },
+  {
+    "revision": "dca85742857f83972562",
+    "url": "​https://hosamf1992.github.io/Weather-App​/static/js/2.1e69c620.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "​https://hosamf1992.github.io/Weather-App​/static/js/2.1e69c620.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f51ba60604725e16efa2",
+    "url": "​https://hosamf1992.github.io/Weather-App​/static/js/main.819c2c57.chunk.js"
+  },
+  {
+    "revision": "147fac732d9075b6753f",
+    "url": "​https://hosamf1992.github.io/Weather-App​/static/js/runtime-main.41f17e94.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "​https://hosamf1992.github.io/Weather-App​/static/media/logo.5d5d9eef.svg"
+  }
+]);
